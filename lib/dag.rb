c865b11b9800c4c0ee31f3a7cc1e224facd37568
Kevin Rutherford <kevin@rutherford-software.com>
2013-04-01 19:46:10 +0100

    A DAG now has vertices

--- lib/dag.rb
+++ lib/dag.rb
@@ -1,7 +1,21 @@
 class DAG
 
+  class Vertex
+    def outgoing_edges
+      []
+    end
+
+    def incoming_edges
+      []
+    end
+  end
+
   def vertices
-    []
+    @vertices ||= []
+  end
+
+  def add_vertex
+    Vertex.new.tap {|v| vertices << v }
   end
 
 end
--- spec/lib/dag_spec.rb
+++ spec/lib/dag_spec.rb
@@ -2,8 +2,23 @@ require 'spec_helper'
 
 describe DAG do
 
-  it 'starts with no vertices' do
-    DAG.new.vertices.should == []
+  context 'when new' do
+    it 'starts with no vertices' do
+      subject.vertices.should be_empty
+    end
+  end
+
+  context 'with one vertex' do
+    it 'has only that vertex' do
+      v = subject.add_vertex
+      subject.vertices.should == [v]
+    end
+
+    it 'has no edges' do
+      v = subject.add_vertex
+      v.outgoing_edges.should be_empty
+      v.incoming_edges.should be_empty
+    end
   end
 
 end
